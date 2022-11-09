bd730e810c6037c899cf8326fcf427f8adb98e30
YiDaoJ <jcxsusan@gmail.com>
2022-11-09 22:23:22 +0100

    add component BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <label>{{ label }}</label>
+  <input
+    class="field"
+    v-bind="$attrs"
+    :placeholder="label"
+    :value="modelValue"
+    @input="
+      $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+    "
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      defautl: '',
+    },
+  },
+})
+</script>
