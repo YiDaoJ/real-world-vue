c4b710cff6aa0e4768e21eb4933866bba125a2c5
YiDaoJ <jcxsusan@gmail.com>
2022-11-10 17:22:22 +0100

    add component BaseSelect

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,70 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <select
+    :value="modelValue"
+    class="field"
+    v-bind="{
+      ...$attrs,
+      onChange: ($event) => $emit('update:modelValue', ($event.target as HTMLSelectElement).value),
+    }"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >
+      {{ option }}
+    </option>
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    options: {
+      type: Array,
+      require: true,
+    },
+  },
+})
+</script>
+
+<style>
+select {
+  width: 100%;
+  height: 52px;
+  padding: 0 24px 0 10px;
+  vertical-align: middle;
+  background: #fff
+    url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 4 5'%3E%3Cpath fill='%23343a40' d='M2 0L0 2h4zm0 5L0 3h4z'/%3E%3C/svg%3E")
+    no-repeat right 12px center;
+  background-size: 8px 10px;
+  border: solid 1px rgba(0, 0, 0, 0.4);
+  border-radius: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+select:focus {
+  border-color: #39b982;
+  outline: 0;
+}
+select:focus::ms-value {
+  color: #000;
+  background: #fff;
+}
+select::ms-expand {
+  opacity: 0;
+}
+</style>
--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>create event forms</div>
+  <h2>create event forms</h2>
   <form>
     <BaseInput v-model="event.title" label="Title" type="text" />
     <BaseInput v-model="event.description" label="Descirption" type="text" />
     <BaseInput v-model="event.location" label="Location" type="text" />
+
+    <BaseSelect
+      label="Select a category"
+      :options="categories"
+      v-model="event.category"
+    />
+
+    <pre>{{ event }}</pre>
   </form>
 </template>
 
@@ -11,10 +19,12 @@
 import { iEvent } from '@/types'
 import { defineComponent } from 'vue'
 import BaseInput from '@/components/BaseInput.vue'
+import BaseSelect from '@/components/BaseSelect.vue'
 
 export default defineComponent({
   components: {
     BaseInput,
+    BaseSelect,
   },
   data() {
     return {
@@ -37,3 +47,43 @@ export default defineComponent({
   },
 })
 </script>
+
+<style>
+.field {
+  margin-bottom: 24px;
+}
+
+form {
+  width: 500px;
+  padding: 0 20px 20px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+}
+
+label,
+input,
+optgroup,
+select,
+textarea {
+  display: inline-flex;
+  font-family: 'Open sans', sans-serif;
+  font-size: 100%;
+  line-height: 1.15;
+  margin: 0;
+}
+
+input {
+  padding: 10px 24px 10px 10px;
+}
+
+label {
+  margin-bottom: 0.25rem;
+  justify-self: flex-start;
+}
+
+pre {
+  text-align: left;
+}
+</style>
